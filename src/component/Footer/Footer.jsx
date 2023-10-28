e6ee81dcc33ae789ed92ff2d58a493f0187d4181
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-10-29 03:12:04 +0600

    footer section deisgn complate

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import footer_img from "../../images/logo_big.png";
+import whatapp from '../../images/whatsapp_icon.png';
+import insta from '../../images/instagram_icon.png'
+import pinterest from '../../images/pintester_icon.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-img">
+        <img src={footer_img} alt="" />
+        <h1>Shopper</h1>
+      </div>
+      <div className="footer_link">
+        <ul className="link_list">
+          <li>Company</li>
+          <li>Product</li>
+          <li>office</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </div>
+      <div className="footer_icons">
+        <ul>
+          <li>
+            <img src={whatapp} alt="" />
+          </li>
+          <li>
+          <img src={insta} alt="" />
+          </li>
+          <li>
+            <img src={pinterest} alt="" />
+          </li> 
+        </ul>
+      </div>
+      <hr />
+      <div>
+        <p>design by bahadur mia shihab</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
