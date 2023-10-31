0eeb943b4d728279333bca00e5578b11fdb037df
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-11-01 01:18:00 +0600

    shopCategory page design complate

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,11 +1,35 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/Context'
+import dropdown_icons from '../images/dropdown_icon.png'
+import Item from '../component/Item/Item'
+import '../Pages/shopCategory.css'
 
-const ShopCategory = ({banner}) => {
+const ShopCategory = (props) => {
   const {all_product} =useContext(ShopContext)
   return (
     <div className='shop_category'>
-        <img src={banner} alt="" />
+        <img className='category-banner' src={props.banner} alt="" />
+        <div className="shopCategory-indexSort">
+          <p><span>showing 1-12</span> out of the products</p>
+          <div className="shopcategory-sort">
+            sort by <img src={dropdown_icons} alt="" />
+          </div>
+        </div>
+        <div className="shopCategory_products">
+          {
+            all_product.map((item,i)=>{
+              if(props.category=== item.category){
+                return <Item  key={i} id={item.id} name={item.name} image = {item.image} new_price = {item.new_price} old_price = {item.old_price} />
+              }
+              else {
+                return null
+              }
+            })
+          }
+        </div>
+        <div className="loadmore">
+          Explore more
+        </div>
     </div>
   )
 }
