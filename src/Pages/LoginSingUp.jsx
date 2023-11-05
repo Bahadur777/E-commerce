cd1908c6ae939a75430f0ade74e2bf0523107243
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2023-11-05 20:24:50 +0600

    Login signUp page desing complate

--- src/Pages/LoginSingUp.jsx
+++ src/Pages/LoginSingUp.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import './LoginSingUp.css'
 
 const LoginSingUp = () => {
   return (
-    <div className='loging-singup'>
-      
+    <div className='login-sinup'>
+      <div className="login-signup-container">
+        <h1>sing Up</h1>
+        <div className="login-signup-fild">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Your Email' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button className='btn'>Continue</button>
+        <div className="area">
+        <p className="loginsingup-login">Already have an account ? <span>Login here</span></p>
+        </div>
+        <div className="login-singup-area">
+          <input type="checkbox" name='' id='' />
+          <p>by continuing , i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
     </div>
   )
 }
